738cafd12f91a80a3873fea0a03e08640246bb5c
jabaraster <jabaraster@gmail.com>
2016-02-23 11:47:32 +0900

    mainte.

--- src/Jabara/Yesod/Middleware.hs
+++ src/Jabara/Yesod/Middleware.hs
@@ -3,8 +3,8 @@ module Jabara.Yesod.Middleware (
     csrfMiddleware
 ) where
 
-import GHC.Base
-import Data.Textual.Encoding (decodeUtf8)
+import Prelude hiding (mapM)
+import GHC.Base (mapM)
 import Data.Text (Text, isSuffixOf)
 import Network.Wai (Request(rawPathInfo))
 import Yesod (Yesod)
@@ -12,13 +12,16 @@ import Yesod.Core (HandlerT, Route, getUrlRender, defaultCsrfMiddleware)
 
 import Jabara.Yesod.Util (getRequestPath)
 
-csrfMiddleware :: Yesod site =>
-    Route site -> HandlerT site IO res -> HandlerT site IO res
-csrfMiddleware logoutRoute  handler = do
+csrfMiddleware :: Yesod site => [Route site] -> HandlerT site IO res -> HandlerT site IO res
+csrfMiddleware uncheckTargetRoutes handler = do
     path <- getRequestPath
-    renderFunc <- getUrlRender
-    logoutPath <- return $ renderFunc logoutRoute
-    if isSuffixOf path logoutPath then
+    unchecks  <- toText uncheckTargetRoutes
+    if any (isSuffixOf path) unchecks then
         handler
       else
         defaultCsrfMiddleware handler
+  where
+    toText :: Yesod site => [Route site] -> HandlerT site IO [Text]
+    toText routes = do
+       renderFunc <- getUrlRender
+       mapM (return . renderFunc) routes
